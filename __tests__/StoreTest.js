20e7480ff2320a5034622fdea9c077eb8e0e1da1
inseong01 <insung940@naver.com>
2025-10-29 12:03:17 +0900

    feat: Store 구현
    - 매장 클래스
      - 올바른 구입금액인지 검증 구역 설정
      - 로또 개수 반환 메서드 구현
      - 관련 테스트 코드 작성

---
+++ __tests__/StoreTest.js
@@ -0,0 +1,26 @@
+import Store from "../src/Store";
+
+describe("매장 클래스 테스트", () => {
+  test("구입금액이 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Store('false');
+    }).toThrow("[ERROR]");
+  });
+
+  test("구입금액이 1,000원으로 나눠지지 않으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Store('1200');
+    }).toThrow("[ERROR]");
+  });
+
+  test("구입금액에 맞춰 로또 개수를 반환한다.", () => {
+    const inputs = ['1000', '2000', '10000'];
+    const outputs = [1, 2, 10];
+
+    inputs.forEach((cash, i) => {
+      const store = new Store(cash);
+
+      expect(store.getLottoAmount()).toBe(outputs[i]);
+    });
+  });
+});
---
+++ src/Store.js
@@ -0,0 +1,21 @@
+class Store {
+  #cash;
+
+  constructor(cash) {
+    this.#validate(cash);
+    this.#cash = Number(cash);
+  }
+
+  #validate(cash) {
+    if (cash % 1000 !== 0) {
+      throw new Error("[ERROR] 천원 단워로 입력해주세요.");
+    }
+  }
+
+  getLottoAmount() {
+    const amount = this.#cash / 1000;
+    return amount;
+  }
+}
+
+export default Store;

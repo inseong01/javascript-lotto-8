d6b2b939abb0b9a790e077904c767391e51c3556
inseong01 <insung940@naver.com>
2025-10-30 11:53:10 +0900

    refactor: 유틸리티 함수명 수정
    - generateNumber -> generateNumbers
      - 배열을 반환해 단수에서 복수로 수정

--- src/utility/generate.js
+++ src/utility/generate.js
@@ -4,7 +4,7 @@ import { MissionUtils } from '@woowacourse/mission-utils';
  * 1-45 사이의 무작위 수로 구성된 숫자 6자리를 생성한다.
  * @returns 숫자 6자리 배열
  */
-export function generateNumber() {
+export function generateNumbers() {
   const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
   return numbers;
 }

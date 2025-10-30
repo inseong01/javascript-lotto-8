8811c6346bbac27d71ffcc9d348e147769c7d04c
inseong01 <insung940@naver.com>
2025-10-30 11:06:49 +0900

    feat: Lotto 기능 구현
    - getNumbers 메서드
      - #numbers는 private이기 때문에 가지고 있는 로또 번호 반환 메서드 적용

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,4 +1,4 @@
-class BonusLotto {
+class Lotto {
   #numbers;
 
   constructor(numbers) {
@@ -8,13 +8,21 @@ class BonusLotto {
 
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
 
     if (new Set(numbers).size !== numbers.length) {
-      throw new Error("[ERROR] 로또 번호는 중복되어서는 안 됩니다.");
+      throw new Error('[ERROR] 로또 번호는 중복되어서는 안 됩니다.');
     }
+  }
+
+  /**
+   * 로또 클래스의 번호를 반환한다.
+   * @returns 로또 번호
+   */
+  getNumbers() {
+    return this.#numbers;
   }
 }
 
-export default BonusLotto;
+export default Lotto;

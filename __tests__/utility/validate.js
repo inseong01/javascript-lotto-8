42b7a2180188da076a5ee3e081a2a852b4dd6d28
inseong01 <insung940@naver.com>
2025-10-31 10:47:15 +0900

    feat: Validator 클래스 구현
    - 구입금액 검증
    - 로또 번호 검증
    - 보너스 번호 검증

---
+++ __tests__/utility/validate.js
@@ -0,0 +1,133 @@
+import Validator from "../../src/utility/validate"
+
+describe('Validator 클래스 테스트', () => {
+  const validator = new Validator();
+
+  describe('validateCash', () => {
+
+    test('인자가 1000원으로 나눠어지지 않으면 예외를 반환한다.', () => {
+      const inputs = ['1200', 'false', '\\n', 'aa', '1', '0', '-1000', '0o1750', '   ', ''];
+
+      inputs.forEach((input) => {
+        function fnBox() {
+          validator.validateCash(input);
+        }
+
+        expect(fnBox).toThrow('[ERROR]');
+      })
+    })
+
+    test('인자가 1000원으로 나눠어지면 예외를 반환하지 않는다.', () => {
+      const inputs = ['1000', '3000', '10000'];
+
+      inputs.forEach((input) => {
+        function fnBox() {
+          validator.validateCash(input);
+        }
+
+        expect(fnBox).not.toThrow('[ERROR]');
+      })
+    })
+  })
+
+  describe('validateLotto', () => {
+    test('인자가 6개가 아니면 예외를 반환한다.', () => {
+      const inputs = ['1,2,3,4,5', '', '1,2,3', '1,2,3,4,5,6,7,8', ',,,,,,,,', ',,,,,,'];
+
+      inputs.forEach((input) => {
+        function fnBox() {
+          validator.validateLotto(input);
+        }
+
+        expect(fnBox).toThrow('[ERROR]');
+      })
+    })
+
+    test('번호가 숫자가 아니면 예외를 반환한다.', () => {
+      const inputs = ['가,나,다,라,마', 'a,b,c,d,e,f', '!,@,#,$,%,^'];
+
+      inputs.forEach((input) => {
+        function fnBox() {
+          validator.validateLotto(input);
+        }
+
+        expect(fnBox).toThrow('[ERROR]');
+      })
+    })
+
+    test('번호가 1-45 사이가 아니면 예외를 반환한다.', () => {
+      const inputs = ['0,1,2,3,4,5', '1,22,33,34,40,52', '-1,2,3,4,-34,-22'];
+
+      inputs.forEach((input) => {
+        function fnBox() {
+          validator.validateLotto(input);
+        }
+
+        expect(fnBox).toThrow('[ERROR]');
+      })
+    })
+
+    test('번호가 중복되는 경우 예외를 반환한다.', () => {
+      const inputs = ['1,1,2,3,4,5', '1,22,33,33,40,52', '2,4,1,4,34,22'];
+
+      inputs.forEach((input) => {
+        function fnBox() {
+          validator.validateLotto(input);
+        }
+
+        expect(fnBox).toThrow('[ERROR]');
+      })
+    })
+
+    test('정상적으로 번호를 입력하면 예외가 반환되지 않는다.', () => {
+      const inputs = [' 1,2,3,4,5 ,6 ', '1 ,22,33, 34,40,45 ', '1,20,3,4,34 ,22'];
+
+      inputs.forEach((input) => {
+        function fnBox() {
+          validator.validateLotto(input);
+        }
+
+        expect(fnBox).not.toThrow('[ERROR]');
+      })
+    })
+  })
+
+  describe('validateBonusNumber', () => {
+    test('인자가 숫자가 아니면 예외를 반환한다.', () => {
+      const inputs = ['false', '', 'a', '가나'];
+
+      inputs.forEach((input) => {
+        function fnBox() {
+          validator.validateBonusNumber(input);
+        }
+
+        expect(fnBox).toThrow('[ERROR]');
+      })
+    })
+
+    test('번호가 1-45 사이가 아니면 예외를 반환한다.', () => {
+      const inputs = ['-1', '0', '46', '100'];
+
+      inputs.forEach((input) => {
+        function fnBox() {
+          validator.validateBonusNumber(input);
+        }
+
+        expect(fnBox).toThrow('[ERROR]');
+      })
+    })
+
+    test('번호가 로또 번호와 중복되면 예외를 반환한다.', () => {
+      const LOTTO = [1, 2, 3, 4, 5, 6];
+      const inputs = ['1', '2', '3', '4'];
+
+      inputs.forEach((input) => {
+        function fnBox() {
+          validator.validateBonusNumber(input, LOTTO);
+        }
+
+        expect(fnBox).toThrow('[ERROR]');
+      })
+    })
+  })
+})
---
+++ src/utility/validate.js
@@ -0,0 +1,45 @@
+class Validator {
+  constructor() {
+    this.message = '[ERROR]';
+  }
+
+  validateCash(string) {
+    if (/\D/.test(string)) throw Error(this.message);
+
+    const number = Number(string);
+
+    if (number === 0) throw Error(this.message);
+
+    if (Math.sign(number) !== 1) throw Error(this.message);
+
+    if (number % 1000 !== 0) throw Error(this.message);
+  }
+
+  validateLotto(string) {
+    // TODO: 유틸리티로 분리
+    const splitedNumbers = string.split(',');
+    const numbers = splitedNumbers.map((num) => num.trim());
+
+    if (numbers.length !== 6) throw Error(this.message);
+
+    if (numbers.some((num) => (/\D/).test(num))) throw Error(this.message);
+
+    if (numbers.some((num) => num > 45 || num < 1)) throw Error(this.message);
+
+    if (new Set(numbers).size !== numbers.length) throw Error(this.message);
+  }
+
+  validateBonusNumber(string, lotto) {
+    if (!string.trim()) throw Error(this.message);
+
+    if (/\D/.test(string)) throw Error(this.message);
+
+    const number = Number(string);
+
+    if (number > 45 || number < 1) throw Error(this.message);
+
+    if (lotto.includes(number)) throw Error(this.message);
+  }
+}
+
+export default Validator;
